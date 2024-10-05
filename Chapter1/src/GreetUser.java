7d6b959a9fe5d0815d6d4789df595536c9972f17
Tushar Shinde <tusharshindets086@gmail.com>
2024-10-05 00:50:26 -0700

    added greet user scanner

---
+++ Chapter1/src/GreetUser.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GreetUser {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+
+            String upperCaseName = name.toUpperCase();
+
+            System.out.println("Hello, " + upperCaseName + ", nice to meet you!");
+
+            scanner.close();
+        }
+    }
