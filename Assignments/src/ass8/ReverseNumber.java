8583ef0d7b54b072e0102aba698cea41a27e5d8f
Tushar Shinde <tusharshindets086@gmail.com>
2024-10-05 01:09:48 -0700

    reverse number

---
+++ Assignments/src/ass8/ReverseNumber.java
@@ -0,0 +1,26 @@
+package ass8;
+
+public class ReverseNumber {
+        public static void main(String[] args) {
+            if (args.length != 1) {
+                System.out.println("Please provide a single number as a command line argument.");
+                return;
+            }
+
+            try {
+                int number = Integer.parseInt(args[0]);
+                int reversed = 0;
+
+                while (number != 0) {
+                    int digit = number % 10;
+                    reversed = reversed * 10 + digit;
+                    number /= 10;
+                }
+
+                System.out.println("Reversed Number: " + reversed);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+
