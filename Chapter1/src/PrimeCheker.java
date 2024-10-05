99f90d7a04f18a94f9a900035e8381adb37ac80b
Tushar Shinde <tusharshindets086@gmail.com>
2024-10-05 02:13:00 -0700

    prime checker

---
+++ Chapter1/src/PrimeCheker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeCheker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // Found a divisor, not prime
+            }
+        }
+        return true; // No divisors found, it is prime
+    }
+}
+
